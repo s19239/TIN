55faf495d44ec562e85b523f93b5bd231220852a
s19239 <61867891+s19239@users.noreply.github.com>
2021-02-04 00:26:15 +0600

    tin 9

---
+++ task9/app.js
@@ -0,0 +1,23 @@
+
+
+var express = require('express');
+var path = require('path');
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
---
+++ task9/routes/index.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const getOperator = (cmd) => {
+    switch (cmd) {
+        case 'add':
+            return (x, y) => x + y;
+        case 'sub':
+            return (x, y) => x - y;
+        case 'mul':
+            return (x, y) => x * y;
+        case 'div':
+            return (x, y) => x / y;
+        default:
+            return 'Error';
+    }
+};
+
+router.get('/', (req, res, next) => {
+    res.render('index', { title: '  My calculator 9a' });
+});
+
+router.post('/', (req, res) => {
+    const result = getOperator(req.body.operator)(Number(req.body.left), Number(req.body.right));
+    console.log(req.body);
+    console.log("Result is" + result);
+    const response = { result };
+    res.send(JSON.stringify(response));
+});
+
+module.exports = router;
